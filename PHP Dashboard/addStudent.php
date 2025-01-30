e90f0f024dea5bf03ac21a3cdebf695b6c4725be
KunKimlong <kimlongkun2412@gmail.com>
2025-01-30 22:12:04 +0700

    Day 21

---
+++ PHP Dashboard/addStudent.php
@@ -0,0 +1,13 @@
+<?php 
+    include('sidebar.php')
+?>
+      <div class="container-fluid">
+       <div class="row">
+        <div class="col-12">
+            <h1>Add Student</h1>
+        </div>
+       </div>
+      </div>
+    <?php 
+        include('footer.php')
+    ?>
---
+++ PHP Dashboard/index.php
@@ -0,0 +1,14 @@
+<?php 
+    include("sidebar.php")
+?>
+      <div class="container-fluid">
+       <div class="row">
+        <div class="col-12">
+            <h1>Welcome to admin dashboard</h1>
+        </div>
+       </div>
+      </div>
+    
+<?php 
+    include("footer.php")
+?>
